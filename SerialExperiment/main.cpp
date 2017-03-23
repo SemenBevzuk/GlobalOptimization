9cf41278e57ef8ede1930fb0b1cdb7a27764c558
Semen Bevzuk <semen.bevzuk@gmail.com>
2017-03-23 19:11:18 +0300

    Серийные испытания

--- SerialExperiment/main.cpp
+++ SerialExperiment/main.cpp
@@ -3,24 +3,6 @@
 #include "vagris.h"
 
 int main(int argc, char* argv[]) {
-	//CMethod im;
-	//im.a = 0;
-	//im.b = 1;
-	//im.eps = 0.001;
-	//im.N = 2;
-	//im.m = 10;
-
-	//im.NumFuncs = 1;
-	//im.SetSearchAreas(0, 1, 0, 1);
-	//im.Funcs[0] = f1;
-	//im.Funcs[1] = f2;
-	//im.Funcs[2] = f3;
-	//im.Funcs[3] = fi;
-	//for (int i = 0; i<im.NumFuncs; i++)
-	//	im.r[i] = 2;
-	//im.Run();
-	//std::cout << "Xmin = " << im.BestTrial.x << " Ymin = " << im.BestTrial.Value << std::endl; 
-
 	FILE *f;
 	fopen_s(&f, "log\\results.csv", "w");
 
@@ -30,23 +12,24 @@ int main(int argc, char* argv[]) {
 	im.b = 1;
 	im.eps = 0.001;
 	im.N = 2;
-	im.m = 12;
+	im.m = 10;
 	im.NumFuncs = 1;
 	im.SetSearchAreas(0, 1.001, 0, 1.001);
 	im.Funcs[0] = random_func;
-	im.r[0] = 2;
+	im.r[0] = 3;
 	Result res;
 	int right = 0;
 	bool IsCorrect;
 	double real_x, real_y, real_value;
-	double eps = 0.001; 
+	double eps = 0.01; 
 	int Correct[101];
 	double delta[101];
-
+	Result results[101];
 	for (nf = 1; nf <= 100; nf++) {
 		IsCorrect = false;
 		set_random(nf);
 		res = im.Run();
+		results[nf] = res;
 		real_x = rand_minimums[(nf - 1) * 2];
 		real_y = rand_minimums[(nf - 1) * 2 + 1];
 		real_value = random_func(real_x,real_y);
@@ -60,9 +43,9 @@ int main(int argc, char* argv[]) {
 			Correct[nf] = 1;
 		}
 		
-		//std::cout << "#" << nf << std::endl;
-		//std::cout << "	Min is : (" <<res.x << "; " << res.y << "), Value = "<<res.Value <<" Points: " << res.points <<std::endl;
-		//std::cout << "	Global min is : (" << real_x << "; " << real_y << "), Value = " << real_value << std::endl;
+		std::cout << "#" << nf << std::endl;
+		std::cout << "	Min is : (" <<res.x << "; " << res.y << "), Value = "<<res.Value <<" Points: " << res.points <<std::endl;
+		std::cout << "	Global min is : (" << real_x << "; " << real_y << "), Value = " << real_value << std::endl;
 
 		fprintf(f, "%f;%f;%f;%f;%f;%f;%d;%d;\n", res.x, res.y, res.Value, real_x, real_y, real_value, res.points, IsCorrect);
 	} 
@@ -73,6 +56,10 @@ int main(int argc, char* argv[]) {
 		if (Correct[i] == 0)
 		{
 			std::cout << "#" << i << " Delta_value = " << delta[i] << std::endl;
+			std::cout << "	Calculated: (x,y) = ( " << results[i].x << "; " << results[i].y << "); Value = " << results[i].Value << std::endl;
+			std::cout << "	Global	  : (x,y) = ( " << rand_minimums[(i - 1) * 2] << "; " << rand_minimums[(i - 1) * 2 + 1]
+				<< "); Value = " << random_func(rand_minimums[(i - 1) * 2], rand_minimums[(i - 1) * 2 + 1]) << std::endl;
+			std::cout << std::endl;
 		}
 	}
 	fclose(f);
